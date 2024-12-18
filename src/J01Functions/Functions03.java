5e85f63c02a2a708505cba31b708c9d3eb707929
nghidadev <mannghi25@gmail.com>
2024-12-18 20:19:52 +0700

    change position of main function, change output

--- src/J01Functions/Functions03.java
+++ src/J01Functions/Functions03.java
@@ -3,20 +3,16 @@ package J01Functions;
 import java.util.Scanner;
 
 /*
-Write a Java program that prints out Welcome to Java n times based on user input.
-Example:
-For n=2, output should be printed on the screen: Welcome to Java\nWelcome to Java
-Input: an integer n entered from keyboard
-Guaranteed constraints:
-1 ≤ n ≤ 100000
-Output: print on the screen Welcome to Java n times.
-Execution time limit: 1 second
+ * Write a Java program that prints out Welcome to Java n times based on user input.
+ * Example:
+ * For n=2, output should be printed on the screen: Welcome to Java\Welcome to Java
+ * Input: an integer n entered from keyboard
+ * Guaranteed constraints:
+ * 1 ≤ n ≤ 100000
+ * Output: print on the screen Welcome to Java n times.
+ * Execution time limit: 1 second
  */
 public class Functions03 {
-    private static void printOut() {
-        System.out.println("Welcome to Java");
-    }
-
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int n = reader.nextInt();
@@ -24,4 +20,9 @@ public class Functions03 {
             printOut();
         }
     }
+
+    private static void printOut() {
+        System.out.println("I am Java Developer");
+    }
+
 }
