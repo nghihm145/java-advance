5907aec5bf01d2a415bd8ff2e758a2d69d7023d3
nghidadev <mannghi25@gmail.com>
2024-12-25 21:48:14 +0700

    update and test

--- src/J04Collections/Collections13.java
+++ src/J04Collections/Collections13.java
@@ -1,5 +1,6 @@
 package J04Collections;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 /*
@@ -13,14 +14,24 @@ Execution time limit: 1 second
 public class Collections13 {
     static String[] distincAdjacent(String[] inputArr) {
         LinkedList<String> aList = new LinkedList<>();
-//        for (int i = 0; i < inputArr.length; i++) {
-//            aList.add(inputArr[i]);
-//        }
-        return aList.toArray(new String[10]);
+        aList.addAll(Arrays.asList(inputArr));
+        return aList.toArray(new String[0]);
     }
 
     public static void main(String[] args) {
         String[] inputArr = {"A", "B", "C", "C", "D"};
-        System.out.println(distincAdjacent(inputArr));
+//        System.out.println(distincAdjacent(inputArr));
+        String[] newArr = distincAdjacent(inputArr);
+        for (String x : newArr){
+            System.out.println(x);
+        }
+        for (int i = 0; i <= 100; i++){
+            System.out.println(i);
+        }
+        System.out.println("======");
+        for (int i = 100; i >= 0; --i){
+            System.out.println(i);
+        }
+
     }
 }
